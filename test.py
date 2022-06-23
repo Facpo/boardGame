c754e6ce85b2059c8d338ac32ffc32857840c57f
francois <francoisauclair@hotmail.ca>
2022-06-23 15:03:02 -0400

    Added function to convert double to offset. x/y vs i/j is broken

--- test.py
+++ test.py
@@ -5,12 +5,18 @@ import neopixel
 
 pixels = neopixel.NeoPixel(board.D18, 132)
 
-def ledIndexFromCoordinates(i,j):
+def ledIndexFromOffsetCoordinates(i,j):
 	if i % 2 == 0:
 		return i * max_cols + j
 	else :
 		return (i + 1) * max_cols - (j + 1) 
 
+def offsetCoordinateFromDouble(hex):
+
+    if hex.y % 2 == 0 :
+        return [hex.x,hex.y/2]
+    else :
+        return [hex.x,(hex.y+1)/2]
 
 max_rows = 12
 max_cols = 11
@@ -25,19 +31,23 @@ for i in range(0,12) :
 			# print (i)
 			# print (j*2)
 			# print ("\n")
-			newhex = hexutil.Hex(i,j*2)
+			newhex = hexutil.Hex(j*2,i)
 		else :
 			# print (i)
 			# print ((j*2)-1)
 			# print ("\n")
-			newhex = hexutil.Hex(i,(j*2)-1)
-		newhex.led = ledIndexFromCoordinates(i,j)
+			newhex = hexutil.Hex((j*2)-1,i)
+		newhex.led = ledIndexFromOffsetCoordinates(i,j)
 		row[j] = newhex
 		grid[i] = row
 
 #print(grid[1][3].led)
 
 pixels[grid[1][3].led] = (255, 0, 0)
+for h in grid[1][3].neighbours() :
+    i,j = offsetCoordinateFromDouble(h)
+    pixels[grid[i][j].led] =  (0, 255, 0)
+
 
 # Filters real physical hexes from hex coordinate space.
 #isOnBoard (i,j) 
